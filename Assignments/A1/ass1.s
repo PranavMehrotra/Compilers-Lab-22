2ffe10ec81f253f247ee2521484c295fbf90f383
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2022-08-07 11:46:20 +0530

    Initial Commit

---
+++ Assignments/A1/ass1.s
@@ -0,0 +1,290 @@
+	.file	"ass1.c"
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Enter the string (all lowrer case): "
+.LC1:
+	.string	"%s"
+.LC2:
+	.string	"Length of the string: %d\n"
+	.align 8
+.LC3:
+	.string	"The string in descending order: %s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$80, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-64(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	-64(%rbp), %rax
+	movq	%rax, %rdi
+	call	length
+	movl	%eax, -68(%rbp)
+	movl	-68(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-32(%rbp), %rdx
+	movl	-68(%rbp), %ecx
+	leaq	-64(%rbp), %rax
+	movl	%ecx, %esi
+	movq	%rax, %rdi
+	call	sort
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.globl	length
+	.type	length, @function
+length:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L5
+.L6:
+	addl	$1, -4(%rbp)
+.L5:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	testb	%al, %al
+	jne	.L6
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	length, .-length
+	.globl	sort
+	.type	sort, @function
+sort:
+.LFB2:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movq	%rdx, -40(%rbp)
+	movl	$0, -8(%rbp)
+	jmp	.L9
+.L13:
+	movl	$0, -4(%rbp)
+	jmp	.L10
+.L12:
+	movl	-8(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %edx
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rcx
+	movq	-24(%rbp), %rax
+	addq	%rcx, %rax
+	movzbl	(%rax), %eax
+	cmpb	%al, %dl
+	jge	.L11
+	movl	-8(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	movb	%al, -9(%rbp)
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	-8(%rbp), %edx
+	movslq	%edx, %rcx
+	movq	-24(%rbp), %rdx
+	addq	%rcx, %rdx
+	movzbl	(%rax), %eax
+	movb	%al, (%rdx)
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movzbl	-9(%rbp), %eax
+	movb	%al, (%rdx)
+.L11:
+	addl	$1, -4(%rbp)
+.L10:
+	movl	-4(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L12
+	addl	$1, -8(%rbp)
+.L9:
+	movl	-8(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L13
+	movq	-40(%rbp), %rdx
+	movl	-28(%rbp), %ecx
+	movq	-24(%rbp), %rax
+	movl	%ecx, %esi
+	movq	%rax, %rdi
+	call	reverse
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	sort, .-sort
+	.globl	reverse
+	.type	reverse, @function
+reverse:
+.LFB3:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movq	%rdx, -40(%rbp)
+	movl	$0, -8(%rbp)
+	jmp	.L15
+.L20:
+	movl	-28(%rbp), %eax
+	subl	-8(%rbp), %eax
+	subl	$1, %eax
+	movl	%eax, -4(%rbp)
+	nop
+	movl	-28(%rbp), %eax
+	movl	%eax, %edx
+	shrl	$31, %edx
+	addl	%edx, %eax
+	sarl	%eax
+	cmpl	%eax, -4(%rbp)
+	jl	.L18
+	movl	-8(%rbp), %eax
+	cmpl	-4(%rbp), %eax
+	je	.L23
+	movl	-8(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	movb	%al, -9(%rbp)
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	-8(%rbp), %edx
+	movslq	%edx, %rcx
+	movq	-24(%rbp), %rdx
+	addq	%rcx, %rdx
+	movzbl	(%rax), %eax
+	movb	%al, (%rdx)
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movzbl	-9(%rbp), %eax
+	movb	%al, (%rdx)
+	jmp	.L18
+.L23:
+	nop
+.L18:
+	addl	$1, -8(%rbp)
+.L15:
+	movl	-28(%rbp), %eax
+	movl	%eax, %edx
+	shrl	$31, %edx
+	addl	%edx, %eax
+	sarl	%eax
+	cmpl	%eax, -8(%rbp)
+	jl	.L20
+	movl	$0, -8(%rbp)
+	jmp	.L21
+.L22:
+	movl	-8(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	-8(%rbp), %edx
+	movslq	%edx, %rcx
+	movq	-40(%rbp), %rdx
+	addq	%rcx, %rdx
+	movzbl	(%rax), %eax
+	movb	%al, (%rdx)
+	addl	$1, -8(%rbp)
+.L21:
+	movl	-8(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L22
+	nop
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	reverse, .-reverse
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
