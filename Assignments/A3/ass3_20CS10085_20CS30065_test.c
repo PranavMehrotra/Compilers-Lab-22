982eabb632f51ecd457ee051672e9e3badfa3e2d
Saransh Sharma <saransh03sharma@gmail.com>
2022-09-06 18:26:46 +0530

    update

--- Assignments/A3/ass3_20CS10085_20CS30065_test.c
+++ Assignments/A3/ass3_20CS10085_20CS30065_test.c
@@ -1,22 +1,52 @@
-#include <stdlib.h>
-
+/* Group members: **Pranav Mehrotra(20CS10085)**
+                  **Saransh Sharma (20CS30065)** */
+#include<iostream>
+#include<stdlib.h>
+#include<string>
 using namespace std;
-typedef struct
+
+/* include headers
+
+
+        */
+
+typedef struct HashNode{//structure defination
+char* key;
+int value;
+}HashNode;
+
+int main(void)
 {
-  if(x>0)while(n);
-  int *Arr;
-  int queue_size, start_id, end_id;
-}QUEUE;//queue structure defined
+    unsigned int a=234533,b,c;//variable declaration
+    float c=2.34e+89,d=3.48,e=234.e90;
+    
+    
+    char d='h';
+    printf("Hello world\n");//print message
+    for(a=0; a<n;i++)
+    {
+        x++;
+    }
 
-//This is a test code
-        /*
-                    to check whitespace
-    */
+    while(a)
+    {
+        b++;
+    }
+    c='\n';
 
-   //To check comments at EOF
-
-for(int i=0;i<n;++i)
-{
-    printf("hello\n");
-    a = '\n';
-}
+    if(variable1==2)
+    {
+        printf("This is variable 1\t\n");
+    }
+    else 
+    {
+        printf("This is variable 2");
+    }
+    switch(1)
+    {
+        case 1:
+            a++;
+        case 2:
+            b++;
+    }return 0;
+}//return from main
