04ad595842db032e116a856d03bb9b8be58187f7
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2022-11-11 21:14:21 +0530

    Many comments added

--- Assignments/A6/ass6_20CS10085_20CS30065_target_translator.cxx
+++ Assignments/A6/ass6_20CS10085_20CS30065_target_translator.cxx
@@ -28,10 +28,12 @@ string function_name = "";
 
 
 // Prints the global information to the assembly file
-void printGlobal(ofstream& sfile) {
+void output_global_info(ofstream& sfile) {
     for(vector<ST_entry*>::iterator it = ST_global.list_ST_entry.begin(); it != ST_global.list_ST_entry.end(); it++) {
         ST_entry* sym = *it;
+        // If object type is 'CHAR'
         if(sym->type.type == CHAR && sym->name[0] != 't') {
+            // If initialised
             if(sym->initial_value != NULL) {
                 sfile << "\t.globl\t" << sym->name << endl;
                 sfile << "\t.data" << endl;
@@ -43,7 +45,9 @@ void printGlobal(ofstream& sfile) {
             else
                 sfile << "\t.comm\t" << sym->name << ",1,1" << endl;
         }
+        // If object type is 'INT'
         else if(sym->type.type == INT && sym->name[0] != 't') {
+            // If initialised
             if(sym->initial_value != NULL) {
                 sfile << "\t.globl\t" << sym->name << endl;
                 sfile << "\t.data" << endl;
@@ -59,26 +63,32 @@ void printGlobal(ofstream& sfile) {
     }
 }
 
-// Function to insert the strings in the assembly file 
-void printStrings(ofstream& sfile) {
+// Function to declare the strings in the assembly file 
+void output_strings(ofstream& sfile) {
+    // Declare the strings
     sfile << ".section\t.rodata" << endl;
     int i = 0;
     for(vector<string>::iterator it = f_strings.begin(); it != f_strings.end(); it++) {
-        sfile << ".LC" << i++ << ":" << endl;
-        sfile << "\t.string " << *it << endl;
+        sfile << ".LC" << i++ << ":" << endl;       // String label
+        sfile << "\t.string " << *it << endl;       // String value
     }
 }
 
-// Function to generate labels for different goto statements of the code
-void setLabels() {
+// Function to generate labels for different jump and branch statements of the code
+void generate_labels() {
     int i = 0;
     for(vector<quad>::iterator it = TAC_list.TAC_quad_list.begin(); it != TAC_list.TAC_quad_list.end(); it++) {
+        // If the quad is a goto statement
         if(it->op == GOTO || (it->op >= GOTO_EQ && it->op <= IF_FALSE_GOTO)) {
+            // target label
             int target = atoi((it->result.c_str()));
+            // If the label is not already present
             if(!labels.count(target)) {
+                // Generate a new label
                 string labelName = ".L" + to_string(label_num++);
                 labels[target] = labelName;
             }
+            // Assign the label to the quad
             it->result = labels[target];
         }
     }
@@ -86,24 +96,30 @@ void setLabels() {
 
 // Function to generate the prologue for the functions
 // It pushes the registers on the stack and allocates space for the local variables
-void generatePrologue(int memBind, ofstream& sfile) {
+void generate_prologue(int memory_bind, ofstream& sfile) {
     int width = 16;
+    // Declare a function name
     sfile << endl << "\t.text" << endl;
     sfile << "\t.globl\t" << function_name << endl;
     sfile << "\t.type\t" << function_name << ", @function" << endl;
     sfile << function_name << ":" << endl;
+    // Push the base pointer on the stack
     sfile << "\tpushq\t" << "%rbp" << endl;
+    // Move the stack pointer to the base pointer
     sfile << "\tmovq\t" << "%rsp, %rbp" << endl;
-    sfile << "\tsubq\t$" << (memBind / width + 1) * width << ", %rsp" << endl;
+    // Allocate space for the local variables
+    sfile << "\tsubq\t$" << (memory_bind / width + 1) * width << ", %rsp" << endl;
 }
 
 // Function to generate assembly code for a given three address quad
-void quadCode(quad q, ofstream& sfile) {
+void generate_assembly(quad q, ofstream& sfile) {
     string strLabel = q.result;
+    // If the quad has a string label
     bool hasStrLabel = (q.result[0] == '.' && q.result[1] == 'L' && q.result[2] == 'C');
     string toPrint1 = "", toPrint2 = "", toPrintRes = "";
     int off1 = 0, off2 = 0, offRes = 0;
 
+    // Search the symbol table for the quad's operands
     ST_entry* location1 = ST->search_lexeme(q.arg1);
     ST_entry* location2 = ST->search_lexeme(q.arg2);
     ST_entry* location3 = ST->search_lexeme(q.result);
@@ -111,6 +127,7 @@ void quadCode(quad q, ofstream& sfile) {
     ST_entry* glb2 = ST_global.search_global_ST(q.arg2);
     ST_entry* glb3 = ST_global.search_global_ST(q.result);
 
+    // If the local symbol table is not the same as global symbol table
     if(ST != &ST_global) {
         if(glb1 == NULL)
             off1 = location1->offset;
@@ -144,9 +161,11 @@ void quadCode(quad q, ofstream& sfile) {
         toPrintRes = q.result;
     }
 
+    // If the quad has a string label
     if(hasStrLabel)
         toPrintRes = strLabel;
 
+    // If quad operation is an assigment operation
     if(q.op == ASSIGN) {
         if(q.result[0] != 't' || location3->type.type == INT || location3->type.type == POINTER) {
             if(location3->type.type != POINTER) {
@@ -168,11 +187,14 @@ void quadCode(quad q, ofstream& sfile) {
             sfile << "\tmovb\t$" << temp << ", " << toPrintRes << endl;
         }
     }
+
+    // If quad operation is Unary Minus
     else if(q.op == U_MINUS) {
         sfile << "\tmovl\t" << toPrint1 << ", %eax" << endl;
         sfile << "\tnegl\t%eax" << endl;
         sfile << "\tmovl\t%eax, " << toPrintRes << endl;
     }
+    // If quad operation is addition
     else if(q.op == ADD) {
         if(q.arg1[0] > '0' && q.arg1[0] <= '9')
             sfile << "\tmovl\t$" << q.arg1 << ", %eax" << endl;
@@ -185,6 +207,7 @@ void quadCode(quad q, ofstream& sfile) {
         sfile << "\taddl\t%edx, %eax" << endl;
         sfile << "\tmovl\t%eax, " << toPrintRes << endl;
     }
+    // If quad operation is subtraction
     else if(q.op == SUB) {
         if(q.arg1[0] > '0' && q.arg1[0] <= '9')
             sfile << "\tmovl\t$" << q.arg1 << ", %edx" << endl;
@@ -198,6 +221,7 @@ void quadCode(quad q, ofstream& sfile) {
         sfile << "\tmovl\t%edx, %eax" << endl;
         sfile << "\tmovl\t%eax, " << toPrintRes << endl;
     }
+    // If quad operation is multiplication
     else if(q.op == MULT) {
         if(q.arg1[0] > '0' && q.arg1[0] <= '9')
             sfile << "\tmovl\t$" << q.arg1 << ", %eax" << endl;
@@ -210,16 +234,20 @@ void quadCode(quad q, ofstream& sfile) {
             sfile << toPrint2 << ", %eax" << endl;
         sfile << "\tmovl\t%eax, " << toPrintRes << endl;
     }
+    // If quad operation is division
     else if(q.op == DIV) {
         sfile << "\tmovl\t" << toPrint1 << ", %eax" << endl;
         sfile << "\tcltd\n\tidivl\t" << toPrint2 << endl;
         sfile << "\tmovl\t%eax, " << toPrintRes << endl;
     }
+    // If quad operation is modulo
     else if(q.op == MOD) {
         sfile << "\tmovl\t" << toPrint1 << ", %eax" << endl;
         sfile << "\tcltd\n\tidivl\t" << toPrint2 << endl;
         sfile << "\tmovl\t%edx, " << toPrintRes << endl;
     }
+
+    // If quad operation are Goto statements
     else if(q.op == GOTO)
         sfile << "\tjmp\t" << q.result << endl;
     else if(q.op == GOTO_LT) {
@@ -334,6 +362,7 @@ void quadCode(quad q, ofstream& sfile) {
         sfile << "\tmovl\t" << toPrint1 << ", %eax" << endl;
         sfile << "\tmovl\t%eax, (%rdx)" << endl;
     }
+    // If quad is a parameter
     else if(q.op == PARAM) {
         int paramSize;
         data_dtype t;
@@ -372,6 +401,7 @@ void quadCode(quad q, ofstream& sfile) {
         }
         params.push(make_pair(ss.str(), paramSize));
     }
+    // If quad is making a call to another function
     else if(q.op == CALL) {
         int numParams = atoi(q.arg1.c_str());
         int totalSize = 0, k = 0;
@@ -443,6 +473,8 @@ void quadCode(quad q, ofstream& sfile) {
             sfile << "\tmovq\t%rax, " << toPrint2 << endl;
         sfile << "\taddq\t$" << totalSize << ", %rsp" << endl;
     }
+
+    // If quad is returning from a function
     else if(q.op == RETURN) {
         if(q.result != "")
             sfile << "\tmovq\t" << toPrintRes << ", %rax" << endl;
@@ -452,13 +484,13 @@ void quadCode(quad q, ofstream& sfile) {
 
 }
 
-// Main function
-void generateTargetCode(ofstream& sfile) {
-    printGlobal(sfile);
-    printStrings(sfile);
-    symbol_table* currFuncTable = NULL;
-    ST_entry* currFunc = NULL;
-    setLabels();
+// Function to generate target assembly code for the given TAC file
+void generate_target(ofstream& sfile) {
+    output_global_info(sfile);
+    output_strings(sfile);
+    symbol_table* curr_function_table = NULL;
+    ST_entry* curr_function = NULL;
+    generate_labels();
 
     for(int i = 0; i < (int)TAC_list.TAC_quad_list.size(); i++) {
         // Print the TAC as comment in the file
@@ -466,42 +498,42 @@ void generateTargetCode(ofstream& sfile) {
         if(labels.count(i))
             sfile << labels[i] << ":" << endl;
 
-        // Necessary tasks for a function
+        // Function definition and required actions
         if(TAC_list.TAC_quad_list[i].op == FUNC_BEG) {
             i++;
             if(TAC_list.TAC_quad_list[i].op != FUNC_END)
                 i--;
             else
                 continue;
-            currFunc = ST_global.search_global_ST(TAC_list.TAC_quad_list[i].result);
-            currFuncTable = currFunc->nested_symbol_table;
-            int takingParam = 1, memBind = 16;
-            ST = currFuncTable;
-            for(int j = 0; j < (int)currFuncTable->list_ST_entry.size(); j++) {
-                if(currFuncTable->list_ST_entry[j]->name == "RETVAL") {
-                    takingParam = 0;
-                    memBind = 0;
-                    if(currFuncTable->list_ST_entry.size() > j + 1)
-                        memBind = -currFuncTable->list_ST_entry[j + 1]->size;
+            curr_function = ST_global.search_global_ST(TAC_list.TAC_quad_list[i].result);
+            curr_function_table = curr_function->nested_symbol_table;
+            ST = curr_function_table;
+            int has_param = 1, memory_bind = 16;
+            for(int j = 0; j < (int)curr_function_table->list_ST_entry.size(); j++) {
+                if(curr_function_table->list_ST_entry[j]->name == "RETVAL") {
+                    has_param = 0;
+                    memory_bind = 0;
+                    if(curr_function_table->list_ST_entry.size() > j + 1)
+                        memory_bind = -curr_function_table->list_ST_entry[j + 1]->size;
                 }
                 else {
-                    if(!takingParam) {
-                        currFuncTable->list_ST_entry[j]->offset = memBind;
-                        if(currFuncTable->list_ST_entry.size() > j + 1)
-                            memBind -= currFuncTable->list_ST_entry[j + 1]->size;
+                    if(!has_param) {
+                        curr_function_table->list_ST_entry[j]->offset = memory_bind;
+                        if(curr_function_table->list_ST_entry.size() > j + 1)
+                            memory_bind -= curr_function_table->list_ST_entry[j + 1]->size;
                     }
                     else {
-                        currFuncTable->list_ST_entry[j]->offset = memBind;
-                        memBind += 8;
+                        curr_function_table->list_ST_entry[j]->offset = memory_bind;
+                        memory_bind += 8;
                     }
                 }
             }
-            if(memBind >= 0)
-                memBind = 0;
+            if(memory_bind >= 0)
+                memory_bind = 0;
             else
-                memBind *= -1;
+                memory_bind *= -1;
             function_name = TAC_list.TAC_quad_list[i].result;
-            generatePrologue(memBind, sfile);
+            generate_prologue(memory_bind, sfile);
         }
 
         // Function which is called when returning from a function
@@ -514,10 +546,11 @@ void generateTargetCode(ofstream& sfile) {
         }
 
         if(function_name != "")
-            quadCode(TAC_list.TAC_quad_list[i], sfile);
+            generate_assembly(TAC_list.TAC_quad_list[i], sfile);
     }
 }
 
+// Main function
 int main(int argc, char* argv[]) {
     ST = &ST_global;
     yyparse();
@@ -532,7 +565,7 @@ int main(int argc, char* argv[]) {
 
     ST = &ST_global;
 
-    generateTargetCode(sfile);      // Function to generate the target assembly code
+    generate_target(sfile);      // Function to generate the target assembly code
 
     sfile.close();
 
