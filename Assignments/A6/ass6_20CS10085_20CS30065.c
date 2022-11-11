970019ad9241a36359de277b8b8c606c2dcc2f2d
Saransh Sharma <saransh03sharma@gmail.com>
2022-11-11 20:26:09 +0530

    Update ass6_20CS10085_20CS30065.c

--- Assignments/A6/ass6_20CS10085_20CS30065.c
+++ Assignments/A6/ass6_20CS10085_20CS30065.c
@@ -4,133 +4,135 @@
 * Compilers Laboratory
 * Assignment 6
 *
-* Library Functions for reading and printing
+*file taken from assignment-2
 */
 
 #include "myl.h"
 #define BUFF_SIZE 100
+#define INT_MAX __INT32_MAX__                       //max integer value
+#define INT_MIN (-INT_MAX - 1)                      //min integer value
 
-// Function to print a string (character array), and return the number of characters printed
 int printStr(char *str)
 {
-    int len = 0;
-    while(str[len] != '\0')     // continue till the null character is found
+    int len = 0;                //intialise len variable to store length of string
+    while(str[len] != '\0')     //increment len until str[len]=='\0'
         len++;
 
-    __asm__ __volatile__ (
-        "movl $1, %%eax \n\t"
-        "movq $1, %%rdi \n\t"
-        "syscall \n\t"
+    __asm__ __volatile__ (          //codes fro printing string
+        "movl $1, %%eax \n\t"       //eax <-- 1 (write) parameters to write
+        "movq $1, %%rdi \n\t"       //rdi <-- 1 (stdout)
+        "syscall \n\t"              //software interrupt
         :
-        :"S"(str), "d"(len)
+        :"S"(str), "d"(len)         //pass the starting address of string and the length of the string to be printed
     );
-    return len;
+    return len;                     //return the length of the string
 }
 
 
-// Function to read an integer from STDIN
 int readInt(int *eP)
 {
-    // ranges for positive and negative integers
-    const long int MAX_INT_POSITIVE = 2147483647;
-    const long int MAX_INT_NEGATIVE = 2147483648;
 
-    char buff[BUFF_SIZE];
-    int len, i = 0, isNegative = 0;
-    long int num = 0;
+    char buff[BUFF_SIZE];                   //create an array to store input from stdin
+    int len, i = 0, sign = 0;
+    long int num = 0;                       //n is stored in a variable of type long ( this is done so that INT_MIN doesn't overflow when we take mod of it i.e. -n would become out of range if we deal with int data types)
     int n;
 
     __asm__ __volatile__ (
-        "movl $0, %%eax \n\t"
-        "movq $0, %%rdi \n\t"
-        "syscall \n\t"
-        :"=a"(len)
-        :"S"(buff), "d"(BUFF_SIZE)
+        "movl $0, %%eax \n\t"               //eax <-- 0 (call read) 
+        "movq $0, %%rdi \n\t"               //rdi <-- 0 read from stdin
+        "syscall \n\t"                      //system interrupt
+        :"=a"(len)                          //to store length of input string (return value of read)
+        :"S"(buff), "d"(BUFF_SIZE)          //the input string will be stored in str of size BUFF
     );
 
-    if(len <= 0) {
-        *eP = ERR;
-        return 0;
-    }
-    if((buff[0] != '+') && (buff[0] != '-') && (buff[0] < '0' || buff[0] > '9')) {    // check for valid first character
+    if(len <= 0) {                          //if length is non-positive i.e. error incurred
         *eP = ERR;
         return 0;
     }
 
-    if(buff[0] == '-' || buff[0] == '+') {      // check for sign if present
+    //check if the first character is valid
+    if((buff[0] != '+') && (buff[0] != '-') && (buff[0] < '0' || buff[0] > '9')) 
+    {    
+        *eP = ERR;
+        return 0;
+    }
+
+    //extract the sign from first digit
+    if(buff[0] == '-' || buff[0] == '+') {      
         if(buff[0] == '-')
-            isNegative = 1;
+            sign = 1;
         i++;
-        if(buff[i] < '0' || buff[i] > '9') {
+        //check if the next digit after sign is invalid
+        if(buff[i] < '0' || buff[i] > '9') 
+        {
             *eP = ERR;
             return 0;
         }
     }
 
-    // continue till a space, tab or newline is found
+    // until end of input is reached
     while(buff[i] != ' ' && buff[i] != '\n' && buff[i] != '\t') {
-        if(buff[i] < '0' || buff[i] > '9') {      // invalid character
+        if(buff[i] < '0' || buff[i] > '9') 
+        {   //check for invalid digits    
             *eP = ERR;
             return 0;
         }
-        int digit = buff[i] - '0';
+        int digit = buff[i] - '0'; //extract the character and convert to digit
 
-        // check for range
-        if(!isNegative && 1L * num * 10 + digit > MAX_INT_POSITIVE) {
+        // check if the number obtained from stdin is out of range
+        if(!sign && 1L * num * 10 + digit > INT_MAX) {
             *eP = ERR;
             return 0;
         }
-        else if(isNegative && 1L * num * 10 + digit > MAX_INT_NEGATIVE) {
+        else if(sign && 1L * num * 10 + digit > INT_MIN) {
             *eP = ERR;
             return 0;
         }
         
-        num = num * 10 + digit;
-        i++;
+        num = num * 10 + digit;             //keep on extracting the digits and multiply them with appropriate powers of 10
+        i++;                                //keep on iterating
     }
 
-    if(isNegative)      // if the original number was negative, multiply by -1
+    if(sign)      //if sign is 1 multiply the number by -1
         num *= -1;
-    n = (int)num;
-    *eP = OK;
-    return n;
+    n = (int)num; //store num in n
+    *eP = OK;     //indicate no error
+    return n;       //return the num
 }
 
 
-// Function to print an integer, and return the number of characters printed
 int printInt(int n)
 {   
-    char buff[BUFF_SIZE];
+    char buff[BUFF_SIZE];   //intialise the character array to store numbers in the form of strings
     int i = 0, j, k;
-    int sign = (n < 0 ? -1 : 1);
+    int sign = (n < 0 ? -1 : 1);//extract the sign
     if(n == 0)
-        buff[i++] = '0';        // handle n = 0 separately
+        buff[i++] = '0';        // n = 0 
     else {
-        if(n < 0)
-            buff[i++] = '-';
+        if(n < 0)               //if n is negative
+            buff[i++] = '-';    //store -1 in first digit
 
         while(n) {
-            buff[i++] = '0' + ((n % 10) * sign);
+            buff[i++] = '0' + ((n % 10) * sign);    //extract ones digit and multiply by sign and keep on storing them
             n /= 10;
         }
 
-        // the number has been placed in the array in reverse order, so we need to reverse it
-        j = (buff[0] == '-' ? 1 : 0);
+        j = (buff[0] == '-' ? 1 : 0);//if the first bit is sign iterate from next bit
         k = i - 1;
-        for(; j < k; j++, k--) {
+        for(; j < k; j++, k--) {//reverse the array
             char temp = buff[j];
             buff[j] = buff[k];
             buff[k] = temp;
         }
     }
-    int size = i;
+    int size = i;//store the size of the string representation of number 
 
     __asm__ __volatile__ (
-        "movl $1, %%eax \n\t"
-        "movq $1, %%rdi \n\t"
-        "syscall \n\t"
+        "movl $1, %%eax \n\t"   //eax <-- 1 (write) parameters to write
+        "movq $1, %%rdi \n\t"   //rdi <-- 1 stdout
+        "syscall \n\t"          //system interrupt
         :
-        :"S"(buff), "d"(size)
+        :"S"(buff), "d"(size)   //pass the string to be printed and len of the string
     );
-    return size;
+    return size;                //return the length of the string printed
 }
